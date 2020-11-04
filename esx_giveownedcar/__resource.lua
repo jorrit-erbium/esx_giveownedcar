4925e34347efe6728072ece752c854fe5a0f8c09
jorrit-erbium <jorrit@molenaarmail.nl>
2020-11-04 22:46:51 +0100

    Update __resource.lua

--- esx_giveownedcar/__resource.lua
+++ esx_giveownedcar/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'made by MEENO'
+description 'made by MEENO, edited by jorrit-erbium'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
